886afdb088ab5cc97752647c139d8fab910b8995
jjinueng <115696442+jjinueng@users.noreply.github.com>
2023-11-20 18:17:28 +0900

    [Silver V] Title: 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(nullptr);
+    ll a, b;
+    cin >> a >> b;
+    cout << lcm(a, b);
+    return 0;
+}

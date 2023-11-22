7a14902d5d95b362b94042c97e9c3d65dc67440c
jjinueng <115696442+jjinueng@users.noreply.github.com>
2023-11-22 15:01:27 +0900

    [Bronze IV] Title: 기찍 N, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define ll long long
+#define endl '\n'
+
+using namespace std;
+
+int main() {
+    fastio;
+    int n;
+    cin >> n;
+    while(n!=0){
+        cout << n << endl;
+        n--;
+    }
+    return 0;
+}

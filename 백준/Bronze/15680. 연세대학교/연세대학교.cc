f4c766a0e352f9ebf773394ef288c7f2dbc3ca8a
jjinueng <115696442+jjinueng@users.noreply.github.com>
2023-11-22 15:09:49 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define ll long long
+#define endl '\n'
+
+using namespace std;
+
+int main() {
+    fastio;
+    int n;
+    cin >> n;
+    if(n==0) cout << "YONSEI";
+    else if(n==1) cout << "Leading the Way to the Future";
+    return 0;
+}

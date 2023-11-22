c6799b8c1a777bbe97d723f4cc5855937bf3f2f1
jjinueng <115696442+jjinueng@users.noreply.github.com>
2023-11-22 15:13:37 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+#define fastio cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define endl '\n'
+using namespace std;
+
+int main() {
+    fastio;
+    int n;
+    cin >> n;
+    cout << n / 11 * 10;
+    return 0;
+}

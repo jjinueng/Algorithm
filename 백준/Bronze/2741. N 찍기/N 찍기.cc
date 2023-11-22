6b52a9c2aba48ca16a9ff0e57c01bb4f3d5833e1
jjinueng <115696442+jjinueng@users.noreply.github.com>
2023-11-22 15:01:21 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+#define fastio cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define endl '\n'
+using namespace std;
+
+int main() {
+    fastio;
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; ++i) {
+        cout << i << endl;
+    }
+}

1b46eab57898e2e23eaf49c2c938e61b20c9bfe7
jjinueng <115696442+jjinueng@users.noreply.github.com>
2023-11-22 15:06:58 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+#define fastio cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define endl '\n'
+using namespace std;
+
+int main() {
+    fastio;
+    string id;
+    cin >> id;
+    cout << id << "??!";
+    return 0;
+}

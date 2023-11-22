d01172178c109b3d17fe685265ce37bb587e5fb6
jjinueng <115696442+jjinueng@users.noreply.github.com>
2023-11-22 15:11:43 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define endl '\n'
+using namespace std;
+
+int main() {
+    fastio;
+    int n;
+    cin >> n;
+
+    for(int i=0; i<(n/4); i++) {
+        cout << "long ";
+    }
+    cout << "int";
+    return 0;
+}

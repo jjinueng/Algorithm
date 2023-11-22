7041d57cfb95265169b359dba0b07fce28e02ae0
jjinueng <115696442+jjinueng@users.noreply.github.com>
2023-11-22 15:00:30 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+#define fastio cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define endl '\n'
+using namespace std;
+
+int main() {
+    fastio;
+    int a, b;
+    cin >> a >> b;
+    cout << a + b;
+    return 0;
+}

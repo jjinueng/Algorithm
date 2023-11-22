cdfa69ea8fd7ceab7c9c6293b4f81002ee504e63
jjinueng <115696442+jjinueng@users.noreply.github.com>
2023-11-22 15:09:38 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 220 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+#define fastio cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define endl '\n'
+using namespace std;
+
+int main() {
+    fastio;
+    int t, a, b;
+    cin >> t;
+    for (int i = 0; i < t; ++i) {
+        cin >> a >> b;
+        cout << a + b << endl;
+    }
+
+    return 0;
+}

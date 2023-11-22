12f05bac6cecfe33f7519c1274621ac767d3ef0b
jjinueng <115696442+jjinueng@users.noreply.github.com>
2023-11-22 14:59:02 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+int main() {
+    int n = 0;
+    scanf("%d", &n);
+    for(int i=0; i<n; i++){
+        for(int j=0; j<=i; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}

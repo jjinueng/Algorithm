1770c4bcf08a5cc7b4bb85d7060f9ca929f71135
jjinueng <115696442+jjinueng@users.noreply.github.com>
2023-11-22 15:04:54 +0900

    [Silver III] Title: 돌 게임 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9659. 돌 게임 5/돌 게임 5.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define ll long long
+#define endl '\n'
+
+using namespace std;
+
+int main() {
+    fastio;
+    ll n;
+    cin >> n;
+    if (n % 2 == 1)cout << "SK";
+    else cout << "CY";
+    return 0;
+}

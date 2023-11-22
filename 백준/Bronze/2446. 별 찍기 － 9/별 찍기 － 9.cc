15485be6e280f884af342a111df2e8a0e405f964
jjinueng <115696442+jjinueng@users.noreply.github.com>
2023-11-22 14:59:43 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define endl '\n'
+using namespace std;
+
+int main() {
+    fastio;
+    int n;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<i; j++){
+            cout << " ";
+        }
+        for(int j=(2*(n-i)-1); j>0; j--){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for(int i=1; i<n; i++){
+
+        for(int j=(n-1); j>i; j--){
+            cout << " ";
+        }
+        for(int j=0; j<(2*i+1); j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+

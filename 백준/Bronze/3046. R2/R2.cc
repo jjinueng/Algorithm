0f53668afdb2771468b9612cdb3450ec76f59809
jjinueng <115696442+jjinueng@users.noreply.github.com>
2023-11-22 15:02:50 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define ll long long
+#define endl '\n'
+
+using namespace std;
+
+int main() {
+    fastio;
+    int r, s;
+    cin >> r >> s;
+    cout << s * 2 - r;
+}

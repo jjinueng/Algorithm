92dd0f837f82a58a81cd9d4d0160fdcd2e44ca92
jjinueng <115696442+jjinueng@users.noreply.github.com>
2023-11-22 15:08:10 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define endl '\n'
+using namespace std;
+
+int main() {
+    fastio;
+
+    char n;
+    cin >> n;
+
+    cout << int(n);
+
+    return 0;
+}

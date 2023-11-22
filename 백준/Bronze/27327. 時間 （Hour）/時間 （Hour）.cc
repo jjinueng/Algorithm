a01b592ee31e87aa7eaafbb8392837585e2e62c9
jjinueng <115696442+jjinueng@users.noreply.github.com>
2023-11-22 15:11:58 +0900

    [Bronze V] Title: 時間 (Hour), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27327. 時間 （Hour）/時間 （Hour）.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define endl '\n'
+using namespace std;
+
+int main() {
+    fastio;
+    int a;
+    cin >> a;
+    cout << a*24;
+    return 0;
+}

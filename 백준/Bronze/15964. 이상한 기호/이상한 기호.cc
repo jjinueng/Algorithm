cbc827b8f82ee4b0001479bc71b63ff3d8a39996
jjinueng <115696442+jjinueng@users.noreply.github.com>
2023-11-22 15:10:10 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define ll long long
+#define endl '\n'
+
+using namespace std;
+
+int main() {
+    fastio;
+    ll a, b;
+    cin >> a >> b;
+    cout << (a + b) * (a - b);
+    return 0;
+}

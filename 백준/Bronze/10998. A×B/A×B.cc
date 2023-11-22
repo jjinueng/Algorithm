da856c2c0ed6a7c913e58940f1641fcee4a45f51
jjinueng <115696442+jjinueng@users.noreply.github.com>
2023-11-22 15:07:29 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define endl '\n'
+using namespace std;
+
+int main() {
+    fastio;
+    int a;
+    int b;
+    cin >> a;
+    cin >> b;
+    cout << a*b;
+    return 0;
+}

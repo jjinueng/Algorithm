498a2ff517dcadf39d6dbdcf8262030588dead04
jjinueng <115696442+jjinueng@users.noreply.github.com>
2023-11-22 15:07:14 +0900

    [Bronze V] Title: A+B - 5, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define endl '\n'
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+    while(1){
+        cin >> a;
+        cin >> b;
+        if(a==0&&b==0)
+            break;
+        cout << a+b << endl;
+    }
+
+    return 0;
+}

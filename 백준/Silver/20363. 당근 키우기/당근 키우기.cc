42b1adc8dc715124de60d3e18ed0193a75842ee6
jjinueng <115696442+jjinueng@users.noreply.github.com>
2023-11-25 16:16:19 +0900

    [Silver IV] Title: 당근 키우기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/20363. 당근 키우기/당근 키우기.cc
+++ 백준/Silver/20363. 당근 키우기/당근 키우기.cc
@@ -3,20 +3,10 @@
 #define endl '\n'
 using namespace std;
 
-void solve(int x, int y) {
-    int ans = x + y;
-    if (x > y) {
-        ans += (y / 10);
-    } else {
-        ans += (x / 10);
-    }
-    cout << ans;
-}
-
 int main() {
     ios_base::sync_with_stdio(false), cin.tie(nullptr);
     int x, y;
     cin >> x >> y;
-    solve(x, y);
+    cout << x + y + min(x, y) / 10;
     return 0;
 }

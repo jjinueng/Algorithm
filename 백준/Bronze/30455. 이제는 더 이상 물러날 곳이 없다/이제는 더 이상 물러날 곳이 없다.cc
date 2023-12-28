271baeb5f29469feee0cf8a75b54aa9d284c5528
jjinueng <115696442+jjinueng@users.noreply.github.com>
2023-12-28 19:35:03 +0900

    [Bronze I] Title: 이제는 더 이상 물러날 곳이 없다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30455. 이제는 더 이상 물러날 곳이 없다/이제는 더 이상 물러날 곳이 없다.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(nullptr);
+    int n;
+    cin >> n;
+    if (n % 2 == 0) cout << "Duck";
+    else cout << "Goose";
+    return 0;
+}

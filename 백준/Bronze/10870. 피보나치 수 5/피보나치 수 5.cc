93e4958971c71c32a8bb04866e0788dd6ad7060b
jjinueng <115696442+jjinueng@users.noreply.github.com>
2024-01-23 15:39:54 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
@@ -3,17 +3,12 @@
 #define ll long long
 using namespace std;
 
-ll fib_arr[100] = {0, 1};
+ll fib_arr[21] = {0, 1};
 
 ll fib(int n) {
-    if (n == 0)
-        return 0;
-    else if (n == 1)
-        return 1;
-    for (int i = 2; i <= n; i++) {
-        fib_arr[i] = fib_arr[i - 1] + fib_arr[i - 2];
-    }
-    return fib_arr[n];
+    if (n == 0) return 0;
+    else if (n > 0 && fib_arr[n] > 0) return fib_arr[n];
+    return fib_arr[n] = fib(n - 1) + fib(n - 2);
 }
 
 int main() {

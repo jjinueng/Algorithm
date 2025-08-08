a387fb09d2068a771b6431793d9f7d99a940d3d4
jjinueng <115696442+jjinueng@users.noreply.github.com>
2025-08-08 17:13:22 +0900

    [Silver V] Title: 투자의 귀재 배주형, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Silver/19947. 투자의 귀재 배주형/투자의 귀재 배주형.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int H = Integer.parseInt(st.nextToken());
+        int Y = Integer.parseInt(st.nextToken());
+
+
+        int[] dp = new int[Y + 1];
+        dp[0] = H;
+
+        for (int i = 1; i <= Y; i++) {
+            dp[i] = (int) (dp[i - 1] * 1.05);
+            if (i >= 3) {
+                dp[i] = Math.max(dp[i], (int) (dp[i - 3] * 1.2));
+            }
+
+            if (i >= 5) {
+                dp[i] = Math.max(dp[i], (int) (dp[i - 5] * 1.35));
+            }
+        }
+
+        bw.write(String.valueOf(dp[Y]));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}

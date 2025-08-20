09386e843aaf845c40d706ee2997b031584f8118
jjinueng <115696442+jjinueng@users.noreply.github.com>
2025-08-20 13:19:24 +0900

    [Bronze V] Title: 카드 게임, Time: 116 ms, Memory: 15880 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int sum = 0;
+
+        for (int i = 0; i < 5; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int score = Integer.parseInt(st.nextToken());
+
+            sum += score;
+        }
+
+        bw.write(sum + "\n");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
